1ceec18f9269f06f2356909d8ebf8e6f42999623
Vladislav Korobeinikov <vladkorobeinikov@gmail.com>
2026-02-08 16:43:39 +0400

    2_producer test

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,9 +1,8 @@
-import json
 import time
-from kafka import KafkaProducer
 import uuid
 from framework.internal.http.account import AccountApi
 from framework.internal.http.mail import MailApi
+from framework.internal.kafka.producer import Producer
 
 
 def test_failed_registration(account: AccountApi, mail: MailApi) -> None:
@@ -36,26 +35,14 @@ def test_success_registration(account: AccountApi, mail: MailApi) -> None:
             raise AssertionError("Email is not found")
 
 
-def test_success_registration_with_kafka_producer(mail: MailApi) -> None:
+def test_success_registration_with_kafka_producer(mail: MailApi, kafka_producer: Producer) -> None:
     base = uuid.uuid4().hex
     message = {
         "login": base,
         "email": f"{base}@mail.ru",
         "password": "123"
     }
-    producer = KafkaProducer(
-        bootstrap_servers=['185.185.143.231:9092'],
-        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
-        acks="all",
-        retries=5,
-        retry_backoff_ms=5000,
-        request_timeout_ms=70000,
-        connections_max_idle_ms=65000,
-        reconnect_backoff_ms=5000,
-        reconnect_backoff_max_ms=10000
-    )
-    producer.send('register-events',message)
-    producer.close()
+    kafka_producer.send('register-events', message)
     for _ in range(10):
         response = mail.find_message(query=base)
         if response.json()['total'] > 0:
