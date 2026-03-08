6a7934fca6dd3b6b910a5c18cfeb48d22f468d69
Vladislav Korobeinikov <vladkorobeinikov@gmail.com>
2026-03-08 23:40:09 +0400

    delete group from Consumer object

--- framework/internal/kafka/consumer.py
+++ framework/internal/kafka/consumer.py
@@ -46,7 +46,6 @@ class Consumer(Singleton):
             bootstrap_servers=self._bootstrap_servers,
             auto_offset_reset="latest",
             value_deserializer=lambda x: json.loads(x.decode("utf-8")),
-            group_id="test-group"
         )
         self._running.set()
         self._ready.clear()
